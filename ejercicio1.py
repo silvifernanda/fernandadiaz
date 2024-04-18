246b3d7df2916979f5d1bd9098be7eb7f6d74d52
MiguelDia <miguelcaicedodiaz10@gmail.com>
2024-04-18 09:48:35 -0500

    mi primer commit

---
+++ ejercicio1.py
@@ -0,0 +1,24 @@
+a=3
+b=8
+suma=a+b
+print("la suma es ",suma)
+
+a=34
+b=58
+resta=a-b
+print("la resta es",resta)
+
+a=23
+b=15
+producto=a*b
+print("la operacion es",producto)
+
+a=23
+b=34
+cociente=a/b
+print("el resultado es",cociente)
+
+a=15
+b=23
+residuo=a%b
+print("el resultado es",residuo)
